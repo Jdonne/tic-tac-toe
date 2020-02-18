772107ea021439b0d4d2ec59d46e6ae1c3092d3c
Eric Tien <eric.tien.13@gmail.com>
2020-02-18 12:48:33 -0500

    add player and win functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//add name entry, reset button and win message
+
 const gameBoard = (() => {
     
     const gBoard = document.querySelector('.board');
@@ -6,28 +8,68 @@ const gameBoard = (() => {
     for (let i = 0; i<board.length; i++){
         const block = document.createElement('div');
         block.className = 'block toFill';
+        block.setAttribute("id", board[i])
         gBoard.appendChild(block);
 
 }
-return {gBoard}
+
 })();
 
 
 
-
+const playerControl =(() =>{
 const block = document.querySelectorAll('.toFill')
 let phase = 0
 
+const win =[ 
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+    ['1', '5', '9'],
+    ['3', '5', '7']
+]
+
+let playerOne=[]
+let playerTwo= []
 const fill = (e) => {
     if (phase ==0 && e.target.classList.contains('toFill')){
         e.target.textContent = 'x'
         e.target.classList.remove('toFill');
+        playerOne.push(e.target.id)
+        
+        for (let i = 0; i<win.length; i++){
+
+            
+            if ((playerOne.includes(win[i][0])) && (playerOne.includes(win[i][1])) && (playerOne.includes(win[i][2]))){
+                console.log('player one wins')
+                    
+                }
+            
+        }
         phase = 1
-    }
+
+        }
+        
     else if (phase == 1 && e.target.classList.contains('toFill') ){
         e.target.textContent = 'o'
         e.target.classList.remove('toFill');
+        playerTwo.push(e.target.id)
+        
+        for (let i = 0; i<win.length; i++){
+
+            
+            if ((playerTwo.includes(win[i][0])) && (playerTwo.includes(win[i][1])) && (playerTwo.includes(win[i][2]))){
+                console.log('player two win')
+                    
+                }
+            
+        }
         phase = 0
+
+
     }
 }
-block.forEach(grid => {grid.addEventListener('click', fill)})
+block.forEach(grid => {grid.addEventListener('click', fill)})})()
